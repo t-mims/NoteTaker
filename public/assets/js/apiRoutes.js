ed2ee1c727cf8cf67c648f17411b79dbebc73028
Tahliyah Mims <mimst97@gmail.com>
2020-10-21 16:58:53 -0500

    basic routes added

--- public/assets/js/apiRoutes.js
+++ public/assets/js/apiRoutes.js
@@ -1,1 +1,16 @@
 //setting up routes
+//needs info/ data to utilize 
+module.exports=function(app){
+    
+    app.get("/api/notes", function (req, res){
+        res.JSON("someData");
+    })
+// ----------------------------
+    app.post("/api/notes", function(req, res){
+        //some response for posting new notes
+    })
+    app.delete("/api/notes/:id", function(req, res){
+        //some response for deleting a selected note using id
+    })
+};
+
--- public/assets/js/htmlRoutes.js
+++ public/assets/js/htmlRoutes.js
@@ -1,1 +1,14 @@
 //setting up routes
+const path= require("path");
+//----------------------------
+//setting up routes
+module.exports=function(app){
+    app.get("*", function (req, res){
+        res.sendFile(path.join(__dirname, "index.html"));
+    })
+
+    app.get("/notes", function(req, res){
+        res.sendFile(path.join(__dirname, "notes.html"));
+    })
+};
+// ----------------------------
